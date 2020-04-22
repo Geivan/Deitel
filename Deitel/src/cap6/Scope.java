008feb3e38c7dbbcf22329aaf4fb86f885d59563
Geivan Martins <47045921+Geivan@users.noreply.github.com>
2020-04-22 20:04:31 -0300

    Update Scope.java

--- Deitel/src/cap6/Scope.java
+++ Deitel/src/cap6/Scope.java
@@ -20,5 +20,14 @@ public class Scope {
         System.out.printf("%nLocal x em main é %d%n", x);
     }
 
-    
+    // cria e inicializa a variaável local x durante cada chamada
+    public static void useLocalVariable() {
+        int x = 25; // inicializada toda vez que useLocalVariable é chamado
+
+        System.out.printf("%nLocal x no método de entrada useLocal é %d%n", x);
+        ++x; // modifica a variável local x desse método
+        System.out.printf("Local x antes de existir o método useLocalVariable is %d%n", x);
+    }
+
+    // modifica o campo x da classe Scope durante a chamada
 }
