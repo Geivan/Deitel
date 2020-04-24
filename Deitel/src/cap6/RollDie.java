1eb3785fd8246e184db605c5cf41805b9d4ad33e
Geivan Martins <47045921+Geivan@users.noreply.github.com>
2020-04-23 21:33:51 -0300

    Update RollDie.java

--- Deitel/src/cap6/RollDie.java
+++ Deitel/src/cap6/RollDie.java
@@ -45,4 +45,4 @@ public class RollDie {
         System.out.println("Face\tFrequency"); // cabeçalhos de saída
         System.out.printf("1\t%d%n2\t%d%n3\t%d%n4\t%d%n5\t%d%n6\t%d%n", frequency1, frequency2, frequency3, frequency4, frequency5, frequency6);
     }
-}
+}  // Fim da classe RollDie
