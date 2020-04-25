2ed917e5b6dcb85a2bf0b13cb49e4eaa56dc534c
Geivan Martins <47045921+Geivan@users.noreply.github.com>
2020-04-25 16:45:10 -0300

    Create DrawSmiley.java

---
+++ Deitel/src/cap6/DrawSmiley.java
@@ -0,0 +1,25 @@
+package cap6;
+// Desenhando um rosto sorridente com cores e formas preenchidas.
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class DrawSmiley {
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        // desenha o rosto
+        g.setColor(Color.YELLOW);
+        g.fillOval(10, 10, 200, 200);
+
+        // desenha os olhos
+        g.setColor(Color.BLACK);
+        g.fillOval(55, 65, 30, 30);
+        g.fillOval(135, 65, 30, 30);
+
+        // desenha a boca para criar um sorriso
+        g.setColor(Color.YELLOW);
+        g.fillRect(50, 110, 120, 30);
+        g.fillOval(50, 120, 120, 40);
+    }
+}
