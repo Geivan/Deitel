128f9ae0eb8924834fd1f220ad5a7dae3220018d
Geivan Martins <47045921+Geivan@users.noreply.github.com>
2020-04-28 18:14:43 -0300

    Create InitiArray2.java

---
+++ Deitel/src/cap7/InitiArray2.java
@@ -0,0 +1,16 @@
+package cap7;
+// Inicializando os elementos de um array com um inicializador de array.
+
+public class InitiArray2 {
+    public static void main(String[] args) {
+        
+        // A lista de inicializador especifica o valor inicial de cada elemento
+        int[] array = { 32, 27, 64, 18, 95, 14, 90, 70, 60, 37 };
+
+        System.out.printf("%s%8s%n", "Index", "Value"); // títulos de coluna
+
+        // gera saída do valor de cada elemento do array
+        for (int counter = 0; counter < array.length; counter++)
+            System.out.printf("%5d%8d%n", counter, array[counter]);
+    }
+}
