5a017bec3f0802ef03e7f7b8d2d6e8e0a35d6c8a
Geivan Martins <47045921+Geivan@users.noreply.github.com>
2020-04-28 17:46:06 -0300

    Create InitArray.java

---
+++ Deitel/src/cap7/InitArray.java
@@ -0,0 +1,15 @@
+package cap7;
+// Inicializando os elementos de um array como valores padrão de zero.
+public class InitArray {
+    public static void main(String[] args) {
+        // delcara array variável e o inicializa com um objeto array
+        int[] array = new int[10]; // cria o objeto array
+        
+        System.out.printf("%s%8s%n", "Index", "Value"); // títulos de coluna
+        
+        // gera saída do valor de cada elemento do array
+        for (int counter = 0; counter < array.length; counter++) {
+            System.out.printf("%5d%8d%n", counter, array[counter]);
+        }
+    }
+}
