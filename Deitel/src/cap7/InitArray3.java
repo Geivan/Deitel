814237f34ff00eb4680d0e9098c6a624fcdbe83c
Geivan Martins <47045921+Geivan@users.noreply.github.com>
2020-04-28 18:37:19 -0300

    Create InitArray3.java

---
+++ Deitel/src/cap7/InitArray3.java
@@ -0,0 +1,19 @@
+package cap7;
+// Calculando os valores a serem colocados nos elementos de um array.
+
+public class InitArray3 {
+    public static void main(String[] args) {
+        final int ARRAY_LENGTH = 10; // declara constante
+        int[] array = new int [ARRAY_LENGTH]; // cria o array
+        
+        // calcula o valor de cada elemento do array
+        for (int counter = 0; counter < array.length; counter++)
+            array[counter] = 2+ 2 * counter;
+        
+        System.out.printf("%s%8s%n", "Index", "Value"); // títulos de coluna
+        
+        //gera saída do valor de cada elemento do array
+        for (int counter = 0; counter < array.length; counter++)
+            System.out.printf("%5d%8d%n", counter, array[counter]);
+    }
+}
