6d4ca2852f6786d1a83db5856482148ece1962fb
Geivan Martins <47045921+Geivan@users.noreply.github.com>
2020-04-29 20:32:11 -0300

    Update BarChart.java

--- Deitel/src/cap7/BarChart.java
+++ Deitel/src/cap7/BarChart.java
@@ -8,6 +8,18 @@ public class BarChart {
         System.out.println("Grade distribution:");
         
         // para cada elemento de array, gera saída de uma barra do gráfico
-        
+        for (int counter = 0; counter < array.length; counter++) {
+            // gera saída do rótulo de barra ( "00-09: ", ..., "90-99: ", "100: ")
+            if (counter == 10) 
+                System.out.printf("%5d: ", 100);
+            else 
+                System.out.printf("%02d-%02d: ", counter * 10, counter * 10 + 9);
+            
+            // imprime a barra de asteriscos
+            for (int stars = 0; stars < array[counter]; stars++)
+                System.out.print("*");
+            
+            System.out.println();
+        }
     }
 }
