627c39d0903483ed2c841df0eae9539623ad2b79
Geivan Martins <47045921+Geivan@users.noreply.github.com>
2020-04-30 15:47:58 -0300

    Create Card.java

---
+++ Deitel/src/cap7/Card.java
@@ -0,0 +1,18 @@
+package cap7;
+// Classe Card representa uma carta de baralho.
+
+public class Card {
+    private final String face; // face da carta ("Ace", "Deuce", ...)
+    private final String suit; // naipe da carta ("Hearts", "Diamonds", ...)
+    
+    // construtor de dois argumentos inicializa face e naipe da carta
+    public Card(String cardFace, String cardSuit) {
+        this.face = cardFace; // inicializa face da carta
+        this.suit = cardSuit; // inicializa naipe da carta
+    }
+    
+    // retorna representação String de Card
+    public String toString() {
+        return face + " of " + suit;
+    }
+}
