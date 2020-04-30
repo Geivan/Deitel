828c7c081b716d734395b4ecf95220f03f5f3a5a
Geivan Martins <47045921+Geivan@users.noreply.github.com>
2020-04-30 14:29:44 -0300

    Update StudentPoll.java

--- Deitel/src/cap7/StudentPoll.java
+++ Deitel/src/cap7/StudentPoll.java
@@ -1,5 +1,28 @@
 package cap7;
+// Programa de análise de enquete.
 
 public class StudentPoll {
-
+    public static void main(String[] args) {
+        // array das respostas dos alunos (mais tipicamente, inserido em tempo de execução)
+        int[] responses = { 1, 2, 5, 4, 3, 5, 2, 1, 3, 3, 1, 4, 3, 3, 3, 2, 3, 3, 2, 14 };
+        int[] frequency = new int[6]; // array de contadores de frequência
+        
+        // para cada resposta, seleciona o elemento de respostas e utiliza esse valor
+        // como índice de frequência para determinar elemento a incrementa
+        for (int answer = 0; answer < responses.length; answer++) {
+            try {
+                ++frequency[responses[answer]];
+            }
+            catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println(e); // invoca o método toString
+                System.out.printf("    responses[%d] = %d%n%n", answer, responses[answer]);
+            }
+        }
+        
+        System.out.printf("%s%10s%n", "Rating", "Frequency");
+        
+        // gera saída do valor de cada elemento do array
+        for (int rating = 1; rating < frequency.length; rating++)
+            System.out.printf("%6d%10d%n", rating, frequency[rating]);
+    }
 }
