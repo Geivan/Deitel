d584617176caca25adca59e56d407590c6e5e8ee
Geivan Martins <47045921+Geivan@users.noreply.github.com>
2020-05-02 20:56:57 -0300

    Update GradeBook.java

--- Deitel/src/cap7/GradeBook.java
+++ Deitel/src/cap7/GradeBook.java
@@ -1,5 +1,9 @@
 package cap7;
+// classe GradeBook utilizando um array para armazenar notas de testes.
 
 public class GradeBook {
-
+    private String courseName; // nome do curso que essa GradeBook representa
+    private int[] grades; // array de notas de aluno
+    
+    // construtor
 }
