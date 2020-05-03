23b8a9c4e56808069779e8b30f6183bbe6876e9e
Geivan Martins <47045921+Geivan@users.noreply.github.com>
2020-05-02 21:00:21 -0300

    Update GradeBook.java

--- Deitel/src/cap7/GradeBook.java
+++ Deitel/src/cap7/GradeBook.java
@@ -6,4 +6,24 @@ public class GradeBook {
     private int[] grades; // array de notas de aluno
     
     // construtor
+    public GradeBook(String courseName, int[] grades){
+        this.courseName = courseName;
+        this.grades = grades;
+    }
+    
+    // método para configurar o nome do curso
+    public void setCourseName(String courseName){
+        this.courseName = courseName;
+    }
+    
+    // método para recuperar o nome do curso
+    public String getCourseName(){
+        return courseName;
+    }
+    
+    // realiza várias operações de dados
+    public void processGrades(){
+        // gera saída de array de notas
+        outputGrades();
+    }
 }
