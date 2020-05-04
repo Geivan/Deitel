5b1256777d462026c923a6beb8b4d5824430ed41
Geivan Martins <47045921+Geivan@users.noreply.github.com>
2020-05-03 21:29:54 -0300

    Update InitiArray2.java

--- Deitel/src/cap7/InitiArray2.java
+++ Deitel/src/cap7/InitiArray2.java
@@ -13,4 +13,4 @@ public class InitiArray2 {
         for (int counter = 0; counter < array.length; counter++)
             System.out.printf("%5d%8d%n", counter, array[counter]);
     }
-}
+} // Fim da classe
