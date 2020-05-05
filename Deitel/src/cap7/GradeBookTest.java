7e3581c54e081946893fda0eff4c2a63c5d14333
Geivan Martins <47045921+Geivan@users.noreply.github.com>
2020-05-04 21:36:31 -0300

    Update GradeBookTest.java

--- Deitel/src/cap7/GradeBookTest.java
+++ Deitel/src/cap7/GradeBookTest.java
@@ -4,5 +4,8 @@ package cap7;
 
 public class GradeBookTest {
     // array de notas de aluno
-    int[] gradesArray = { 87, 68, 94, 100, 83, 78, 85, 91, 76, 87 };
+    public static void main(String[] args) {          
+        int[] gradesArray = { 87, 68, 94, 100, 83, 78, 85, 91, 76, 87 };
+        
+    }
 }
