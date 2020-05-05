a1ca63378535aa2a2fedc859c672595227ae7402
Geivan Martins <47045921+Geivan@users.noreply.github.com>
2020-05-04 21:39:42 -0300

    Update GradeBookTest.java

--- Deitel/src/cap7/GradeBookTest.java
+++ Deitel/src/cap7/GradeBookTest.java
@@ -10,6 +10,9 @@ public class GradeBookTest {
         
         GradeBook mayGradeBook = new GradeBook(
             "CS101 Introduction to Java Programming", gradesArray);
+        System.out.printf("Welcome to the grade book for%n%s%n%n",
+                myGradeBook.getCourseName());
+        myGradeBook.processGrades();
         
     }
 }
