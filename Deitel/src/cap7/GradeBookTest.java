339173cf5bc9ffb5238ba80eb3c3ce70a3d9949d
Geivan Martins <47045921+Geivan@users.noreply.github.com>
2020-05-04 21:35:36 -0300

    Update GradeBookTest.java

--- Deitel/src/cap7/GradeBookTest.java
+++ Deitel/src/cap7/GradeBookTest.java
@@ -1,5 +1,8 @@
 package cap7;
+// GradeBookTest cria um objeto GradeBook utilizando um array de notas,
+// e, então, invoca o método processGrades para analisá-las.
 
 public class GradeBookTest {
-
+    // array de notas de aluno
+    int[] gradesArray = { 87, 68, 94, 100, 83, 78, 85, 91, 76, 87 };
 }
