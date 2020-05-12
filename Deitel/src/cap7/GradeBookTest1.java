0f75af44c0601867ae5e42d09bc50d4ab34f9f91
Geivan Martins <47045921+Geivan@users.noreply.github.com>
2020-05-11 21:03:38 -0300

    Update GradeBookTest1.java

--- Deitel/src/cap7/GradeBookTest1.java
+++ Deitel/src/cap7/GradeBookTest1.java
@@ -1,6 +1,6 @@
 package cap7;
 // GradeBookTest1 cria o objeto GradeBook1 utilizando um array bidimensional
-// das notas e, então,
+// das notas e, então, invoca o método processGrades para analisá-las.
 
 public class GradeBookTest1 {
 
