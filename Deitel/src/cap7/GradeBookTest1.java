bdca0dcc32a00ba4f6b465be92ff97d4d81111a4
Geivan Martins <47045921+Geivan@users.noreply.github.com>
2020-05-11 21:11:50 -0300

    Update GradeBookTest1.java

--- Deitel/src/cap7/GradeBookTest1.java
+++ Deitel/src/cap7/GradeBookTest1.java
@@ -6,5 +6,12 @@ public class GradeBookTest1 {
     // método main inicia a execução de programa
     public static void main(String[] args) {
         // array bidimensional de notas de aluno
+        int[][] gradesArray = {{87, 96, 70}, {68, 87, 90}, {94, 100, 90},
+                               {100, 81, 82}, {83, 65, 85}, {78, 87, 65},
+                               {85, 75, 83}, {91, 94, 100}, {76, 72, 84},
+                               {87, 93, 73}};        
+        
+        GradeBook1 myGradeBook1 = new GradeBook1(
+            "CS101 Introduction to Java Programing", gradesArray);
     }
 }
