7eca4555673354a44b19ffbcd07424b37df3270b
Geivan Martins <47045921+Geivan@users.noreply.github.com>
2020-05-12 22:06:01 -0300

    Update VarargsTest.java

--- Deitel/src/cap7/VarargsTest.java
+++ Deitel/src/cap7/VarargsTest.java
@@ -3,7 +3,7 @@ package cap7;
 
 public class VarargsTest {
     // calcula a média
-    public static double avarage(double... numbers){
+    public static double average(double... numbers){
         double total = 0.0;
         
         // calcula total utilizando a instrução for aprimorada
@@ -19,6 +19,10 @@ public class VarargsTest {
         double d3 = 30.0;
         double d4 = 40.0;
         
+        System.out.printf("d1 = %.1f%nd2 = %.1f%nd3 = %.1f%nd4 = %.1f%n%n", d1, d2, d3, d4);
         
+        System.out.printf("Avarage of d1 and d2 is %.1f%n", average(d1, d2));
+        System.out.printf("Average of d1, d2 and d3 is %.1f%n", average(d1, d2, d3));
+        System.out.printf("Average of d1, d2, d3 and d4 is %.1f%n", average(d1, d2, d3, d4));
     }
 }
