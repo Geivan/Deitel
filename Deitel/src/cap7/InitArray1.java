43a022c0b0d25d02818d60de8b182997e308807b
Geivan Martins <47045921+Geivan@users.noreply.github.com>
2020-05-13 22:07:18 -0300

    Update InitArray1.java

--- Deitel/src/cap7/InitArray1.java
+++ Deitel/src/cap7/InitArray1.java
@@ -7,7 +7,7 @@ public class InitArray1 {
         if(args.length != 3)
             System.out.printf(
                 "Error: Please re-enter the entire command, including\n" +
-                "an array size, initial value and increment./n");
+                "an array size, initial value and increment.\n");
         else{
             // obtém o tamanho do array a partir do primeiro argumento de linha de comando
             int arrayLength = Integer.parseInt(args[0]);
@@ -20,6 +20,12 @@ public class InitArray1 {
             // calcula valor de cada elemento do array
             for(int counter = 0; counter < array.length; counter++)
                 array[counter] = initialValue + increment * counter;
+            
+            System.out.printf("%s%8s\n", "Index", "Value");
+            
+            // exibe o valor e o índice de array
+            for(int counter = 0; counter < array.length; counter++)
+                System.out.printf("%5d%8d\n", counter, array[counter]);
         }
     }
 }
