1fa2d13ec65fcfa5628f480587c4aee0e32f7d7b
Geivan Martins <47045921+Geivan@users.noreply.github.com>
2020-05-14 22:54:26 -0300

    Update ArrayManipulations.java

--- Deitel/src/cap7/ArrayManipulations.java
+++ Deitel/src/cap7/ArrayManipulations.java
@@ -6,6 +6,7 @@ public class ArrayManipulations {
     public static void main(String[] args) {
         // classifica doubleArray em ordem crescente
         double[] doubleArray = {8.4, 9.3, 0.2, 7.9, 3.4 };
-        Array.sort(doubleArray);
+        Arrays.sort(doubleArray);
+        System.out.printf("%.1f ", value);
     }
 }
