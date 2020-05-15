a74ace7632072e2ec725ddfa8a8b4c8cdf5f2d1f
Geivan Martins <47045921+Geivan@users.noreply.github.com>
2020-05-14 23:15:08 -0300

    Update ArrayManipulations.java

--- Deitel/src/cap7/ArrayManipulations.java
+++ Deitel/src/cap7/ArrayManipulations.java
@@ -7,7 +7,7 @@ public class ArrayManipulations {
         // classifica doubleArray em ordem crescente
         double[] doubleArray = {8.4, 9.3, 0.2, 7.9, 3.4 };
         Arrays.sort(doubleArray);
-        System.out.printf("/ndoubleArray: ");
+        System.out.printf("\ndoubleArray: ");
         
         for(double value : doubleArray)
             System.out.printf("%.1f ", value);
@@ -23,5 +23,33 @@ public class ArrayManipulations {
         System.arraycopy(intArray, 0, intArrayCopy, 0, intArray.length);
         displayArray(intArray, "intArray");
         displayArray(intArrayCopy, "intArrayCopy");
+        
+        // verifica a igualdade de intArray e intArrayCopy
+        boolean b = Arrays.equals(intArray, intArrayCopy);
+        System.out.printf("\n\nintArray %s intArrayCopy\n", (b ? "==" : "!="));
+        
+        //pesquisa o valor 5 em intArray
+        int location = Arrays.binarySearch(intArray, 5);
+        
+        if(location >= 0)
+            System.out.printf("Found 5 at element %d in intArray\n", location);
+        else
+            System.out.println("5 not found in intArray");
+        
+        // pesquisa o valor 8763 em intArray
+        location = Arrays.binarySearch(intArray, 8763);
+        
+        if(location >= 0)
+            System.out.printf("Found 8763 at element %d in intArray\n", location);
+        else
+            System.out.println("8763 not found in intArray");
+    }
+    
+    // gera saída de valores em cada array
+    public static void displayArray(int[] array, String description){
+        System.out.printf("\n%s: ", description);
+        
+        for(int value : array)
+            System.out.printf("%d ", value);
     }
 }
