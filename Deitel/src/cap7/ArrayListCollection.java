5f5af122987799baf8df4aeb1c68ebb89d3e65ad
Geivan Martins <47045921+Geivan@users.noreply.github.com>
2020-05-15 23:10:15 -0300

    Update ArrayListCollection.java

--- Deitel/src/cap7/ArrayListCollection.java
+++ Deitel/src/cap7/ArrayListCollection.java
@@ -19,5 +19,32 @@ public class ArrayListCollection {
         
         // exibe as cores usando for aprimorada no método display
         display(items, "\nDisplay list contents with enhanced for statement:");
+        
+        items.add("green"); // adiciona "green" ao fim da lista                
+        items.add("yellow"); // adiciona "yellow" ao fim da lista
+        display(items, "List with two new elements:");
+        
+        items.remove("yellow"); // remove o primeiro yellow
+        display(items, "Remove first instace of yellow:");
+        
+        items. remove(1); // remove o item no índice 1
+        display(items, "Remove second list element (green):");
+        
+        // verifica se um valor está na List
+        System.out.printf("\"red\" is %sin the list\n", (items.contains("red") ? "" : "not "));
+        
+        // exibe o número de elementos na List
+        System.out.printf("Size: %s\n", items.size());
+    }
+    
+    // exibe elementos do ArrayList no console
+    public static void display(ArrayList<String> items, String header){
+        System.out.printf(header); // exibe o cabeçalho
+        
+        // exibe cada elemento em itens
+        for(String item : items)
+            System.out.printf(" %s", item);
+        
+        System.out.println();
     }
 }
