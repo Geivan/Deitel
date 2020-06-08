a4dc56da0aec9583816bbb7e6e124f7617a993c6
Geivan Martins <47045921+Geivan@users.noreply.github.com>
2020-06-07 21:50:06 -0300

    Update DrawRainbow.java

--- Deitel/src/cap7/DrawRainbow.java
+++ Deitel/src/cap7/DrawRainbow.java
@@ -1,6 +1,10 @@
 package cap7;
 // Desenhando um arco-íris com arcos e um array de cores.
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
 
-public class DrawRainbow {
-
+public class DrawRainbow extends JPanel {
+	// define as cores índigo e violeta
+	
 }
