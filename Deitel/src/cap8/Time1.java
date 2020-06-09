442907b74bbf333e7081f4f7ec600954df31a72f
Geivan Martins <47045921+Geivan@users.noreply.github.com>
2020-06-08 22:34:54 -0300

    Update Time1.java

--- Deitel/src/cap8/Time1.java
+++ Deitel/src/cap8/Time1.java
@@ -15,5 +15,18 @@ public class Time1 {
 		}
 		
 		this.hour = hour;
+		this.minute = minute;
+		this.second = second;
+	}
+	
+	// converte em String no formato de data/hora universal (HH:MM:SS)
+	public String toUniversalString() {
+		return String.format("%02d:%02d:%02d", hour, minute, second);
+	}
+	
+	// converte em String no formato padrão de data/hora (H:MM:SS AM ou PM)
+	public String toString() {
+		return String.format("%d:%02d:%02d %s", ((hour == 0 || hour == 12) ? 12 : hour % 12),
+							  minute, second, (hour < 12 ? "AM" : "PM"));
 	}
 }
